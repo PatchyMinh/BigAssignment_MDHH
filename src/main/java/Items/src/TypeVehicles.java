f6b9d0cb0c75d2c75262ce2c627d3cfa5e69c6b1
KoPhaiDung <dungvqht1@gmail.com>
2026-04-12 20:45:59 +0700

    ss

---
+++ src/main/java/Items/src/TypeVehicles.java
@@ -0,0 +1,6 @@
+public class TypeVehicles implements ItemsFactory{
+    @Override
+    public Items createItems(ItemsAttributes request) {
+        return new Vehicles(request.getOwner(), request.getStartingPrice(), request.getDescription(), request.getBrand(), request.getMileage(), request.getVehicleID());
+    }
+}
