90a62a4320550fd5803605428748d5d9834872df
phuynguyen162-hub <phuynguyen162@gmail.com>
2026-04-16 00:00:54 +0700

    UI Login

---
+++ Source/src/login/src/main/java/com/example/login/Login.java
@@ -0,0 +1,22 @@
+package com.example.login;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Login extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("login.fxml"));
+        Scene scene = new Scene(loader.load());
+
+        stage.setTitle("Login");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
